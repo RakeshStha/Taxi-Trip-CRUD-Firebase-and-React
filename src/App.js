56e2ed00a856d66ef481309de61b262dcbd52416
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-05-29 22:16:01 +0545

    Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Footer from './component/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-        Hello world
+        <Footer/>
     </div>
   );
 }
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "../Footer/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="top-footer-container">
+          <div className="Aboutus">
+            <h1>About Us</h1>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              porro recusandae assumenda eaque vero. Cumque quaerat error fugiat
+              nam atque odio laboriosam, corrupti suscipit excepturi incidunt.
+              Possimus facilis quis eum harum voluptates veniam corrupti
+              consequatur!
+            </h3>
+          </div>
+          <div className="Address">
+            <h1>Address</h1>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              porro recusandae assumenda eaque vero. Cumque quaerat error fugiat
+              nam atque odio laboriosam, corrupti suscipit excepturi incidunt.
+              Possimus facilis quis eum harum voluptates veniam corrupti
+              consequatur!
+            </h3>
+          </div>
+          <div className="Contact">
+            <h1>Contact</h1>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              porro recusandae assumenda eaque vero. Cumque quaerat error fugiat
+              nam atque odio laboriosam, corrupti suscipit excepturi incidunt.
+              Possimus facilis quis eum harum voluptates veniam corrupti
+              consequatur!
+            </h3>
+          </div>
+        </div>
+        <div className="bottom-footer-container">
+          <div className="Copyright">
+            <h3>Ⓒ Taxi Trip -All Rights Reserved</h3>
+          </div>
+          <div className="Privacy-policy">
+            <h3>Privacy Policy</h3>
+          </div>
+          <div className="Terms-use">
+            <h3>Terms of Use</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
